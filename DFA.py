8f9acc434b62075fb58f4d73486faaeca7a11dcd
Adithya Olivannan <ad837157@dal.ca>
2024-10-07 13:03:04 -0300

    Completed implementing a DFA that tries to check for valid math equations. Bracket mismatch can still happen

--- DFA.py
+++ DFA.py
@@ -1,13 +1,103 @@
 import os
+
+class State:
+    state_name = ''
+    is_final = False
+    # stores all the states that can transition from this state
+    valid_transitions = {}
+
+    # constructor
+    def __init__(self, state_name, is_final):
+        self.state_name = state_name
+        self.is_final = is_final
+
+    # assigns the valid transitions that this state can make
+    def assign_valid_transitions(self, valid_transitions):
+        self.valid_transitions = valid_transitions
+
+    # returns whether the transition that is being made from this state is valid
+    def can_transition(self, transition_state):
+        if transition_state in self.valid_transitions:
+            return True
+        return False
+
+
 class DFA:
+    # all states in the DFA
+    # all states in the DFA
+    q0 = State("", False)
+    q1 = State("", False)
+    q2 = State("", False)
+    q3 = State("", False)
+
+    # initial setup
+    states = [q0, q1, q2, q3]
+    state = "Valid"
+    initial_state = State("", False)
+    current_state = State("", False)
+
+    # what are operators and what are brackets
+    operators = ["+", "-", "*", "/"]
+    brackets = ["[", "(", ")", "]"]
+
+    #constructor
     def __init__(self):
-        print("")
+
+        # initializing states
+        self.q0 = State("q0", False)
+        self.q1 = State("q1", True)
+        self.q2 = State("q2", False)
+        self.q3 = State("q3", True)
+
+        # assigning valid transitions to states
+        self.q0.assign_valid_transitions({self.q1, self.q2, self.q3})
+        self.q1.assign_valid_transitions({self.q1, self.q2, self.q3})
+        self.q2.assign_valid_transitions({self.q1, self.q3})
+        self.q3.assign_valid_transitions({self.q1, self.q2, self.q3})
+
+        # initializing initial and current state variables
+        self.states = [self.q0, self.q1, self.q2, self.q3]
+        self.initial_state = self.q0
+        self.current_state = self.initial_state
+
+    # sets the current state to the initial state
     def reset(self):
-        print()
+        self.current_state = self.initial_state
+
+    # transitions from state to state. If a transition is invalid, sets the state param to Invalid
     def transition(self, char):
-        print()
+        # reading a number
+        if char.isdigit():
+            if self.current_state.can_transition(self.q1):
+                self.state = "Valid"
+            else:
+                self.state = "Invalid"
+            return self.state
+
+        # reading an operator
+        if char in self.operators:
+            if self.current_state.can_transition(self.q2):
+                self.state = "Valid"
+            else:
+                self.state = "Invalid"
+            return self.state
+
+        # reading a bracket
+        if char in self.brackets:
+            if self.current_state.can_transition(self.q3):
+                self.state = "Valid"
+            else:
+                self.state = "Invalid"
+            return self.state
+
+    # returns if the current state is final
+    def is_accepting(self):
+        if self.current_state.is_final:
+            return True
+        return False
+
+    # don't know what this does yet
     def is_valid(self, string):
-        #some implementation
         print()
 
     # Your code ends here, do not modify code below
